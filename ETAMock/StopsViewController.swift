8152e33802725a1f33a7423e5cea59f749a03c17
dfreemanRIIS <dfreeman@riis.com>
2017-03-27 16:23:01 -0400

    Setup for southbound/northbound

--- ETAMock/StopsViewController.swift
+++ ETAMock/StopsViewController.swift
@@ -14,18 +14,17 @@ class StopsViewController: UIViewController, UITableViewDataSource, UITableViewD
     @IBOutlet weak var segmentButton: UISegmentedControl!
     var stopNames:[String] = []
     let urlString = "http://ec2-204-236-211-33.compute-1.amazonaws.com:8080/companies/3/routes/87/northbound/weekday/1/stops"
+    let urlStringSouth = "http://ec2-204-236-211-33.compute-1.amazonaws.com:8080/companies/3/routes/87/southbound/weekday/1/stops"
     
     override func viewWillAppear(_ animated: Bool) {
         //Fetch
         let jsonFetcher = JSONfetcher()
         let url = jsonFetcher.getSourceUrl(apiUrl: urlString)
         let jsonString = jsonFetcher.callApi(url: url)
-        print(jsonString)
         
         //Parse
         let parser = customJSONparser()
         stopNames = parser.getDirectionOneStops(jsonString)
-        print(stopNames)
         
         //Reload
         self.tableView.reloadData()
@@ -52,5 +51,16 @@ class StopsViewController: UIViewController, UITableViewDataSource, UITableViewD
         cell.textLabel?.text = self.stopNames[indexPath.row]
         
         return cell
+    }
+
+    @IBAction func directionButtonClick(_ sender: UISegmentedControl) {
+        switch segmentButton.selectedSegmentIndex {
+        case 0:
+            print("northbound")
+        case 1:
+            print("southbound")
+        default:
+            break
+        }
     }
 }
